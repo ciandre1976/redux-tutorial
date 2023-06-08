7816b87b8b0f27900786ccb97963811a7373bb35
AleksandarStanisic01 <a.stanisic.codealkonost@gmail.com>
2023-06-08 22:22:06 +0200

    fix

--- starter/src/features/cart/cartSlice.js
+++ starter/src/features/cart/cartSlice.js
@@ -15,8 +15,21 @@ const cartSlice = createSlice({
     clearCart: (store) => {
       store.cartItems = [];
     },
+    removeItems: (store, action) => {
+      store.cartItems = store.cartItems.filter(
+        (item) => item.id !== action.payload
+      );
+    },
+    increase: (store, { payload }) => {
+      const cardItem = store.cartItems.find((item) => item.id === payload);
+      cardItem.amount = cardItem.amount + 1;
+    },
+    decrease: (store, { payload }) => {
+      const cardItem = store.cartItems.find((item) => item.id === payload);
+      cardItem.amount = cardItem.amount - 1;
+    },
   },
 });
 
-export const { clearCart } = cartSlice.actions;
+export const { clearCart, removeItems, increase, decrease } = cartSlice.actions;
 export default cartSlice.reducer;
