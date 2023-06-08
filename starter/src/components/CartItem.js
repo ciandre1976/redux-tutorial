f70f680fd0dce299e9f3a647c4531b1c7d7c5e07
AleksandarStanisic01 <a.stanisic.codealkonost@gmail.com>
2023-06-08 20:33:09 +0200

    cartItm

--- starter/src/components/CartItem.js
+++ starter/src/components/CartItem.js
@@ -1,7 +1,23 @@
-import React from "react";
+import { ChevronDown, ChevronUp } from "../icons";
 
-
-export default function CartItem() {
-
-  return <div>CartItem</div>;
+export default function CartItem({ id, img, title, price, amount }) {
+  return (
+    <article className="cart-item">
+      <img src={img} alt={title} />
+      <div>
+        <h4>{title}</h4>
+        <h4 className="item-price">${price}</h4>
+        <button className="remove-btn">remove</button>
+      </div>
+      <div>
+        <button className="amount-btn">
+          <ChevronUp />
+        </button>
+        <p className="amount">0</p>
+        <button className="amount-btn">
+          <ChevronDown />
+        </button>
+      </div>
+    </article>
+  );
 }
